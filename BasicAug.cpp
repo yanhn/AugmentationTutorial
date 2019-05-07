fd0e7ca2d03bf08d8eb24a9a148d5b4463ccd394
Johnny <johnny@JohnnydeMacBook-Pro.local>
2019-05-07 19:44:16 +0800

    add cpp data augmentation framework.

---
+++ BasicAug.cpp
@@ -0,0 +1,95 @@
+//
+// Created by nvidia on 19-5-7.
+//
+
+#include "BasicAug.h"
+
+namespace hawk{
+    ImageData::ImageData(){
+        img = cv::Mat();
+        labelId = -1;
+        boxInfos = {};
+    }
+
+    BasicAugmentation::BasicAugmentation(){
+        _augmentationOpName = "BasicAug";
+    }
+
+    BasicAugmentation::~BasicAugmentation(){
+    }
+
+    bool BasicAugmentation::transform(ImageData& imgData){
+        return false;
+    }
+
+    std::string BasicAugmentation::getTransformName(){
+        return _augmentationOpName;
+    }
+
+    // StableResizeAugmentation
+    StableResizeAugmentation::StableResizeAugmentation(int outWidth, int outHeight){
+        _outWidth = outWidth;
+        _outHeight = outHeight;
+        _augmentationOpName = "StableResizeAug";
+    }
+
+    StableResizeAugmentation::~StableResizeAugmentation(){
+
+    }
+
+    bool StableResizeAugmentation::transform(ImageData& imgData){
+        int inputWidth = imgData.img.cols;
+        int inputHeight = imgData.img.rows;
+        float ratioWidth = inputWidth / _outWidth;
+        float ratioHeight = inputHeight / _outHeight;
+        if (ratioWidth >= ratioHeight){
+            int resizedHeight = int(inputHeight / ratioWidth);
+            int heightPadding = (_outHeight - resizedHeight) / 2;
+            cv::resize(imgData.img, imgData.img, cv::Size2d(_outWidth, resizedHeight));
+            cv::copyMakeBorder(imgData.img, imgData.img, heightPadding, _outHeight - resizedHeight, 0, 0, cv::BORDER_CONSTANT);
+            return true;
+        } else{
+            int resizedWidth = int(inputWidth / ratioHeight);
+            int widthPadding = (_outWidth - resizedWidth) / 2;
+            cv::resize(imgData.img, imgData.img, cv::Size2d(resizedWidth, _outHeight));
+            cv::copyMakeBorder(imgData.img, imgData.img, 0, 0, widthPadding, _outWidth - resizedWidth, cv::BORDER_CONSTANT);
+            return true;
+        }
+    }
+
+    // SubMeanDivideVarAugmentation
+    SubMeanDivideVarAugmentation::SubMeanDivideVarAugmentation(std::vector<float> means, std::vector<float> vars){
+        assert(means.size() == vars.size());
+        assert(means.size() >= 1);
+        if (means.size() > 1){
+            _isSingleChannel = false;
+        } else {
+            _isSingleChannel = true;
+        }
+        _means = means;
+        _vars = vars;
+        _augmentationOpName = "SubMeanDivideVarsAug";
+    }
+
+    SubMeanDivideVarAugmentation::~SubMeanDivideVarAugmentation(){
+
+    }
+
+    /*
+     * input: imgData with img of type CV_8UC(n); after transform, change to CV_32FC(n)
+     */
+    bool SubMeanDivideVarAugmentation::transform(ImageData& imgData){
+        if (_isSingleChannel){
+            imgData.img.convertTo(imgData.img, CV_32FC1, 1/_vars[0], -_means[0]);
+            return true;
+        }else{
+            std::vector<cv::Mat> bgrPlanes;
+            cv::split(imgData.img, bgrPlanes);
+            for (int i = 0; i < bgrPlanes.size(); i ++ ){
+                bgrPlanes[i].convertTo(bgrPlanes[i], CV_32FC1, 1/_vars[i], -_means[i]);
+            }
+            cv::merge(bgrPlanes, imgData.img);
+            return true;
+        }
+    }
+}
---
+++ BasicAug.h
@@ -0,0 +1,69 @@
+//
+// Created by nvidia on 19-5-7.
+//
+
+#ifndef TRTINFER_BASICAUG_H
+#define TRTINFER_BASICAUG_H
+
+#endif //TRTINFER_BASICAUG_H
+
+#include <vector>
+#include <opencv.hpp>
+
+namespace hawk{
+    /*
+     * Description: image data struct
+     * labelId: used in image classification application.
+     * boxInfos: used in detection application, 2d vector, inner vector in format of: [xmin, ymin, xmax, ymax, classId]
+     * img: cv::Mat image
+     */
+    struct ImageData{
+        int labelId;
+        std::vector<std::vector<float>> boxInfos;
+        cv::Mat img;
+        ImageData();
+    };
+
+    /*
+     * Description: data augmentation interface. It is a pure virtual class.
+     * Function: transform is a pure virtual method, with 1 in-out parameter.
+     */
+    class BasicAugmentation{
+    public:
+        BasicAugmentation();
+        virtual ~BasicAugmentation();
+        virtual bool transform(ImageData& imgData);
+        std::string getTransformName();
+    protected:
+        std::string _augmentationOpName;
+    };
+
+    /*
+     * Description: Used to do image resize. Stable means keep w/h ratio unchanged, so no distortion introduced
+     * Constructor:
+     */
+    class StableResizeAugmentation:BasicAugmentation{
+    public:
+        StableResizeAugmentation(int outWidth, int outHeight);
+        ~StableResizeAugmentation();
+        bool transform(ImageData& imgData);
+    private:
+        int _outWidth;
+        int _outHeight;
+    };
+
+    /*
+     * Description: Used to do sub mean and divide vars oprations.
+     * Convert input rtype from
+     */
+    class SubMeanDivideVarAugmentation:BasicAugmentation{
+    public:
+        SubMeanDivideVarAugmentation(std::vector<float> means, std::vector<float> vars);
+        ~SubMeanDivideVarAugmentation();
+        bool transform(ImageData& imgData);
+    private:
+        std::vector<float> _means;
+        std::vector<float> _vars;
+        bool _isSingleChannel;
+    };
+}
